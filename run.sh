bdfba2b8a8b6ede9a7a2157ddf7e811e3da0f9a8
Luis Simon <lsimon@godaddy.com>
2025-03-12 15:17:09 -0600

    chore: include argument to the run script to provide the template to run

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ if [ $1 == "--help" ]; then
   echo "Usage: sh run.sh <profile> <region>"
   exit 1
 fi
-if [ $# -ne 2 ]; then
-  echo "Usage: sh run.sh <profile> <region>"
+if [ $# -ne 3 ]; then
+  echo "Usage: sh run.sh <profile> <region> <template>"
   exit 1
 fi
 if [ -z "$1" ]; then
@@ -20,12 +20,18 @@ if [ -z "$2" ]; then
   echo "Region is empty"
   exit 1
 fi
+if [ -z "$3" ]; then
+  echo "Template is empty"
+  exit 1
+fi
 profile=$1
 region=$2
+template=$3
 
 # Print the profile and region
 echo "Profile: $profile"
 echo "Region: $region"
+echo "Template: $template"
 
 # Create an EC2 key pair named "EC2KeyName" and save the private key to a file.
 # If it already exists, delete it first.
@@ -40,7 +46,7 @@ aws ec2 create-key-pair  --region $region  --profile $profile --key-name EC2KeyN
 aws cloudformation deploy \
   --region $region \
   --profile $profile \
-  --template-file template.yaml \
+  --template-file $template \
   --stack-name coe-aws-obs-poc-stack \
   --parameter-overrides KeyName=EC2KeyName InstanceType=t3.micro \
   --capabilities CAPABILITY_NAMED_IAM
